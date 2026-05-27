efc9d1fd46cbe5be45125dfcb677f621d08d34f3
katariyaprit <pritkatariya098123@gmail.com>
2026-05-27 19:04:30 +0530

    chanes

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
+  base: '/',  // આ ઉમેરો
   build: {
     chunkSizeWarningLimit: 1600,
+    assetsDir: 'assets',
+    outDir: 'dist',
   },
   server: {
     hmr: {
